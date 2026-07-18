cf3bf91771e8f67a7b076b583882f969b5da0f8e
trenjie03 <trenjie007@gmail.com>
2026-07-19 00:39:01 +0800

    Fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: '/trenjie03.github.io/',   // Change "portfolio" to your repo name
-})
+});
