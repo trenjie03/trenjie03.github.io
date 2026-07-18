656cba055114fbb5485d8ee5cb861377731b8d12
trenjie03 <trenjie007@gmail.com>
2026-07-19 00:35:11 +0800

    repush

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/trenjie03.github.io/',   // Change "portfolio" to your repo name
+})
